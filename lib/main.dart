7da861a8a1f084cf15ede13648fe4c513d3a9394
YuvrajBankolia <yuvrajbankolia@gmail.com>
2025-02-18 18:11:56 +0530

    commit 1

---
+++ lib/main.dart
@@ -0,0 +1,201 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'BLE Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(title: 'Flutter BLE Demo'),
+      );
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({super.key, required this.title});
+
+  final String title;
+  final List<BluetoothDevice> devicesList = <BluetoothDevice>[];
+
+  @override
+  MyHomePageState createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  BluetoothDevice? _connectedDevice;
+  List<BluetoothService> _services = [];
+  String _sensorData = '0'; // Variable to hold sensor data
+
+  _addDeviceTolist(final BluetoothDevice device) {
+    if (!widget.devicesList.contains(device)) {
+      setState(() {
+        widget.devicesList.add(device);
+      });
+    }
+  }
+
+  _initBluetooth() async {
+    var subscription = FlutterBluePlus.onScanResults.listen(
+      (results) {
+        if (results.isNotEmpty) {
+          for (ScanResult result in results) {
+            _addDeviceTolist(result.device);
+          }
+        }
+      },
+      onError: (e) => ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(e.toString()),
+        ),
+      ),
+    );
+
+    FlutterBluePlus.cancelWhenScanComplete(subscription);
+
+    await FlutterBluePlus.adapterState
+        .where((val) => val == BluetoothAdapterState.on)
+        .first;
+
+    await FlutterBluePlus.startScan();
+
+    await FlutterBluePlus.isScanning.where((val) => val == false).first;
+    FlutterBluePlus.connectedDevices.map((device) {
+      _addDeviceTolist(device);
+    });
+  }
+
+  @override
+  void initState() {
+    () async {
+      var status = await Permission.location.status;
+      if (status.isDenied) {
+        final status = await Permission.location.request();
+        if (status.isGranted || status.isLimited) {
+          _initBluetooth();
+        }
+      } else if (status.isGranted || status.isLimited) {
+        _initBluetooth();
+      }
+
+      if (await Permission.location.status.isPermanentlyDenied) {
+        openAppSettings();
+      }
+    }();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // Disconnect from device when app is closed
+    _connectedDevice?.disconnect();
+    super.dispose();
+  }
+
+  void _connectToDevice(BluetoothDevice device) async {
+    await device.connect();
+    setState(() {
+      _connectedDevice = device;
+    });
+
+    // Discover services
+    List<BluetoothService> services = await device.discoverServices();
+    for (var service in services) {
+      for (var characteristic in service.characteristics) {
+        if (characteristic.properties.notify) {
+          await characteristic.setNotifyValue(true);
+          characteristic.value.listen((value) {
+            setState(() {
+              _sensorData = utf8.decode(value);
+            });
+          });
+        }
+      }
+    }
+  }
+
+  ListView _buildListViewOfDevices() {
+    List<Widget> containers = <Widget>[];
+    for (BluetoothDevice device in widget.devicesList) {
+      containers.add(
+        SizedBox(
+          height: 50,
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                    Text(device.platformName == ''
+                        ? '(unknown device)'
+                        : device.advName),
+                    Text(device.remoteId.toString()),
+                  ],
+                ),
+              ),
+              TextButton(
+                child: const Text(
+                  'Connect',
+                  style: TextStyle(color: Colors.black),
+                ),
+                onPressed: () async {
+                  FlutterBluePlus.stopScan();
+                  _connectToDevice(device);
+                },
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return ListView(
+      padding: const EdgeInsets.all(8),
+      children: <Widget>[
+        ...containers,
+      ],
+    );
+  }
+
+  StatelessWidget _buildView() {
+    if (_connectedDevice != null) {
+      return DataPage(sensorData: _sensorData);
+    }
+    return _buildListViewOfDevices();
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: _buildView(),
+      );
+}
+
+class DataPage extends StatelessWidget {
+  final String sensorData;
+
+  DataPage({required this.sensorData});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Data Page'),
+      ),
+      body: Center(
+        child: Text(
+          'Ultrasonic Sensor Data: $sensorData cm',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
